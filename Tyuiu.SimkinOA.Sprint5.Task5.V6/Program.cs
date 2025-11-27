f2f598254062d0a75b4384f1c07645f0b79ae81e
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-27 20:35:57 +0500

    Task5

--- Tyuiu.SimkinOA.Sprint5.Task5.V6/Program.cs
+++ Tyuiu.SimkinOA.Sprint5.Task5.V6/Program.cs
@@ -6,33 +6,32 @@ namespace Tyuiu.SimkinOA.Sprint5.Task5.V6
         static void Main(string[] args)
         {
             DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Симкин О.А.| РППб-25-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #5                                                            *");
+            Console.WriteLine("* Тема:                                                                *");
+            Console.WriteLine("* Задание #5                                                           *");
+            Console.WriteLine("* Вариант #6                                                           *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич  | РППб-25-1                         *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
 
-            Console.Title = "Спринт #5 | Выполнил: Симкин О.А. | РППб-25-1";
-            Console.WriteLine("###########################################################################");
-            Console.WriteLine("# Спринт #5                                                               #");
-            Console.WriteLine("# Тема: Чтение набора данных из текстового файла                          #");
-            Console.WriteLine("# Задание #5                                                              #");
-            Console.WriteLine("# Вариант #6                                                              #");
-            Console.WriteLine("# Выполнил Симкин Олег Андреевич | РППб-25-1                              #");
-            Console.WriteLine("###########################################################################");
-            Console.WriteLine("# УСЛОВИЕ:                                                                #");
-            Console.WriteLine("# Найти среднее значение всех вещественных чисел в файле. Полученный      #");
-            Console.WriteLine("# результат вывести на консоль. У вещественных значений округлить до трёх #");
-            Console.WriteLine("# знаков после запятой.                                                   #");
-            Console.WriteLine("###########################################################################");
-            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
-            Console.WriteLine("###########################################################################");
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V6.txt";
+            Console.WriteLine("данные находятся в файле: " + path);
 
-            string path = @"C:\DataSprint5\InPutDataFileTask5V6.txt";
-            Console.WriteLine("Данные находятся в файле: " + path);
-
-            Console.WriteLine("###########################################################################");
-            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
-            Console.WriteLine("###########################################################################");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
 
             double res = ds.LoadFromDataFile(path);
-            Console.WriteLine("Среднее значение всех вещественных = " + res);
+
+            Console.WriteLine("среднее значение = " + res);
+            
             Console.ReadKey();
         }
     }
-}
+} 
