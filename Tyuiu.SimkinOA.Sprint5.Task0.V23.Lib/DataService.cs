89e90c02f71c9215ab2affa113cb38c2ff95490e
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-27 13:49:13 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SimkinOA.Sprint5.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SimkinOA.Sprint5.Task0.V23.Lib
+{
+    public class DataService : ISprint5Task0V23
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
+            double z = Math.Round((1 + Math.Pow(x, 3)) / Math.Pow(x, 2), 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint5.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SimkinOA.Sprint5.Task0.V23.Test;
+namespace Tyuiu.SimkinOA.Sprint5.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\rausc\source\repos\Tyuiu.RaushKN.Sprint5\Tyuiu.RaushKN.Sprint5.Task0.V23\bin\Debug\net8.0\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
