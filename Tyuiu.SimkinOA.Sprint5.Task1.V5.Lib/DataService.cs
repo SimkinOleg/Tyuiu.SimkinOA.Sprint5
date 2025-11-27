38cfed0a0d5e0b2d517a8ab3f100f5382c781911
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-27 15:36:52 +0500

    Task1

--- Tyuiu.SimkinOA.Sprint5.Task1.V5.Lib/DataService.cs
+++ Tyuiu.SimkinOA.Sprint5.Task1.V5.Lib/DataService.cs
@@ -3,36 +3,33 @@ namespace Tyuiu.SimkinOA.Sprint5.Task1.V5.Lib
 {
     public class DataService : ISprint5Task1V5
     {
-        public class DataService : ISprint5Task1V5
+        public string SaveToFileTextData(int startValue, int stopValue)
         {
-            public string SaveToFileTextData(int startValue, int stopValue)
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            if (fileExists)
             {
-                string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
-                FileInfo fileinfo = new FileInfo(path);
-                bool fileExists = fileinfo.Exists;
-                if (fileExists)
-                {
-                    File.Delete(path);
-                }
-                double y;
-                string strY;
-                for (int x = startValue; x <= stopValue; x++)
-                {
-                    y = Math.Round(5 - 3 * x + (1 + Math.Sin(x)) / (2 * x - 0.5), 2);
-                    strY = Convert.ToString(y);
-                    if (x != stopValue)
-                    {
-                        File.AppendAllText(path, strY + Environment.NewLine);
-
-                    }
-                    else
-                    {
-                        File.AppendAllText(path, strY);
-                    }
-
-                }
-                return path;
+                File.Delete(path);
             }
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(5 - 3 * x + (1 + Math.Sin(x)) / (2 * x - 0.5), 2);
+                strY = Convert.ToString(y);
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, strY + Environment.NewLine);
+
+                }
+                else
+                {
+                    File.AppendAllText(path, strY);
+                }
+
+            }
+            return path;
         }
     }
 }
