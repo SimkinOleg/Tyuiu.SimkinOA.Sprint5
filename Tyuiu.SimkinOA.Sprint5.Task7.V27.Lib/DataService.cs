e9d1f98f8ddf682a48a4ee907df23343b4f17927
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-27 20:54:18 +0500

    Task7

---
+++ Tyuiu.SimkinOA.Sprint5.Task7.V27.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.SimkinOA.Sprint5.Task7.V27.Lib
+{
+    public class DataService : ISprint5Task7V27
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string fileContent = File.ReadAllText(path);
+            string modifiedContent = Regex.Replace(fileContent, " ", "");
+            string outputDirectory = Path.GetTempPath();
+            string outputFileName = "OutPutDataFileTask7V26.txt";
+            string outputFilePath = Path.Combine(outputDirectory, outputFileName);
+            File.WriteAllText(outputFilePath, modifiedContent);
+            return outputFilePath;
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint5.Task7.V27/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.SimkinOA.Sprint5.Task7.V27.Lib;
+namespace Tyuiu.SimkinOA.Sprint5.Task7.V27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Симкин О.А. | РППб-25-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #5                                                            *");
+            Console.WriteLine("* Тема:                                                                *");
+            Console.WriteLine("* Задание #7                                                           *");
+            Console.WriteLine("* Вариант #27                                                          *");
+            Console.WriteLine("* Выполнил: Симкин Олег Андреевич  | РППб-25-1                         *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V26.txt";
+            var result = ds.LoadDataAndSave(path);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
