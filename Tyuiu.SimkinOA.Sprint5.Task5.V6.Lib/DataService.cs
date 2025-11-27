1b417610a64dc8822bffa1368a674e1f20f1aade
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-27 20:42:56 +0500

    Task5

--- Tyuiu.SimkinOA.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.SimkinOA.Sprint5.Task5.V6.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.IO;
+using System.Globalization;
 using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.SimkinOA.Sprint5.Task5.V6.Lib
 {
@@ -6,26 +7,24 @@ namespace Tyuiu.SimkinOA.Sprint5.Task5.V6.Lib
     {
         public double LoadFromDataFile(string path)
         {
+            string strx = File.ReadAllText(path);
+            strx = strx.Replace(".", ",");
+            string[] strings = strx.Split(' ');
             double res = 0;
-            double count = 0;
-            string data = File.ReadAllText(path);
-            string[] strings = data.Split(' ');
-
-            var number = new List<double>();
-            foreach (string s in strings)
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
             {
-                double nunber = double.Parse(s, CultureInfo.InvariantCulture);
-                number.Add(nunber);
-            }
+                foreach (string i in strings)
+                {
+                    if (Math.Round(Convert.ToDouble(i), 0) != Convert.ToDouble(i))
+                    {
+                        count++;
+                        res += Convert.ToDouble(i);
+                    }
 
-            foreach (double nunber in number)
-            {
-                res += nunber;
-                count++;
+                }
             }
-
-            res = res / count;
-            return Math.Round(res, 3);
+            return Math.Round(res / count, 3);
         }
     }
 }
