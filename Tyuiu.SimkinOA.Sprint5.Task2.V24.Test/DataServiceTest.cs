36b2cfb8f142ebe5349ee306227bb513e21b3d93
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-11-27 16:21:28 +0500

    Task2

---
+++ Tyuiu.SimkinOA.Sprint5.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+namespace Tyuiu.SimkinOA.Sprint5.Task2.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
